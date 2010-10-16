2d6cbe13e459c25be3570d561de55080c57f6b69
Shane Liebling <shanel@gmail.com>
2010-10-16 00:04:11 -0400

    trying to get a new gift event form working

--- app/models/gift_event.rb
+++ app/models/gift_event.rb
@@ -3,7 +3,9 @@ class GiftEvent
 
   key :gift_admin_email, String, :required => true, :format => RFC822::EmailAddress
   key :gift_admin_key, String, :required => true
+  key :gift_admin_name, String
   key :gift_key, String, :required => true
+  key :gift_title, String, :required => true
   key :gift_name, String, :required => true
   key :gift_price, Float, :required => true
   key :gift_link, String
