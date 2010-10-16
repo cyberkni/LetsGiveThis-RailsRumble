c3af939cedd9f283999379855fb796840bb3553c
Dan Van Derveer <dan@van.derveer.com>
2010-10-16 17:20:42 -0400

    fix uploads

--- app/controllers/planner/gift_events_controller.rb
+++ app/controllers/planner/gift_events_controller.rb
@@ -12,6 +12,7 @@ class Planner::GiftEventsController < ActionController::Base
   end
 
   def create
+    debugger
     @gift_event = GiftEvent.new(params[:gift_event])
     respond_to do |format|
       if @gift_event.save
