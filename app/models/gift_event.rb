94c12db3d3659fa9a0f4c597d419898ec7b5069b
Dan Van Derveer <dan@van.derveer.com>
2010-10-16 00:07:54 -0400

    add auto token generation

--- app/models/gift_event.rb
+++ app/models/gift_event.rb
@@ -2,13 +2,20 @@ class GiftEvent
   include MongoMapper::Document
 
   key :gift_admin_email, String, :required => true, :format => RFC822::EmailAddress
-  key :gift_admin_key, String, :required => true
-  key :gift_key, String, :required => true
+  key :gift_admin_token, String, :required => true, :unique => true
+  key :access_token, String, :required => true, :unique => true
   key :gift_name, String, :required => true
   key :gift_price, Float, :required => true
   key :gift_link, String
   key :gift_file_path, String
 
-#  many :contributions
+  before_validation :create_access_tokens
+  many :contributions
 
+  private
+  
+  def create_access_tokens
+    self.gift_admin_token = ActiveSupport::SecureRandom.hex(16)
+    self.access_token = ActiveSupport::SecureRandom.hex(16)
+  end
 end
