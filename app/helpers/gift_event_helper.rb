e9ee51fe120da2bcc335472fb961b98284a43330
Dan Van Derveer <dan@van.derveer.com>
2010-10-16 12:26:59 -0400

    typo fix

--- app/helpers/gift_event_helper.rb
+++ app/helpers/gift_event_helper.rb
@@ -1,9 +1,9 @@
 module GiftEventHelper
   def generate_gift_title(gift_event)
     if gift_event.gift_link != ''
-      "<h2><a href='#{gift_event.gift_link}'>#{gift_event.gift_title}</a></h2>"
+      "<h2><a href='#{gift_event.gift_link}'>#{gift_event.gift_name}</a></h2>"
     else
-      "<h2>#{gift_event.gift_title}</h2>"
+      "<h2>#{gift_event.gift_name}</h2>"
     end
   end
 end
