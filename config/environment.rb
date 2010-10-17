f85c5615645e20db1b31c7d9291895b1883a3182
Shane Liebling <shanel@gmail.com>
2010-10-16 21:39:56 -0400

    turning off tls

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Letsgivethis::Application.initialize!
+
+# Turn off auto TLD
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
