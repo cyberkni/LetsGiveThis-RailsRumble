28c2491907ce9d163b9f8177a9e52ad280032387
Shane Liebling <shanel@gmail.com>
2010-10-17 08:47:09 -0400

    cleaning up resend link output

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -11,6 +11,10 @@ class Notifier < ActionMailer::Base
   def resend_admin_link(gift_events)
     @gift_events = gift_events
     @admin_email = @gift_events.first.gift_admin_email
+    @links = ''
+    @gift_events.map do |event|
+      @links += "#{event.gift_name} - http://www.letsgivethis.com/planner/event/#{event.gift_admin_token}\n"
+    end
     mail(:to => @gift_events.first.gift_admin_email, :subject => "Your forgotten Let's Give This info!") do |format|
       format.text { render "resend_admin_link" }
     end
