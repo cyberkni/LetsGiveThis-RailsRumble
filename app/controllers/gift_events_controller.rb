91c63ca5707b1f17fa4fabe37273066603f92291
Shane Liebling <shanel@gmail.com>
2010-10-16 22:03:31 -0400

    couple edits to fix empty emails - I hope

--- app/controllers/gift_events_controller.rb
+++ app/controllers/gift_events_controller.rb
@@ -18,7 +18,7 @@ class GiftEventsController < ActionController::Base
   
   def admin_recovery
     @gift_events = GiftEvent.find_all_by_gift_admin_email(params[:admin_email])
-    Notifier.resend_admin_link(@gift_events)
+    Notifier.resend_admin_link(@gift_events).deliver
     logger.info "mailed to #{@gift_events.first.gift_admin_email}"
     render
   end
