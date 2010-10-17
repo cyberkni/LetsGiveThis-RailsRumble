6a4572849ad20a85f9463174899d60afc253a0d7
Shane Liebling <shanel@gmail.com>
2010-10-16 21:56:07 -0400

    couple edits to fix empty emails - I hope

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,14 +3,18 @@ class Notifier < ActionMailer::Base
   
   def over_the_line(gift_event)
     @gift_event = gift_event
-    mail(:to => gift_event.gift_admin_email, :subject => "You're Over The Line!")
+    mail(:to => @gift_event.gift_admin_email, :subject => "You're Over The Line!") do |format|
+      format.text { render "over_the_line" }
+    end
   end
   
   def resend_admin_link(gift_events)
     @gift_events = gift_events
     @admin_email = @gift_events.first.gift_admin_email
-    @admin_tokens = gift_events.map { |x| "http://letsgivethis.com/event/#{x.gift_admin_token}" }
-    mail(:to => gift_events.first.gift_admin_email, :subject => "Your forgotten Let's Give This info!")
+    @admin_tokens = @gift_events.map { |x| "http://letsgivethis.com/event/#{x.gift_admin_token}" }
+    mail(:to => @gift_events.first.gift_admin_email, :subject => "Your forgotten Let's Give This info!") do |format|
+      format.text { render "resend_admin_link" }
+    end
   end
   
 end
